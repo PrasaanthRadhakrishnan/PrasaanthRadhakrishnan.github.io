e2e93e6511d124f4d90d84df525ff1479461d1f6
PrasaanthRadhakrishnan <101669175+PrasaanthRadhakrishnan@users.noreply.github.com>
2022-06-01 21:11:26 -0700

    Updated HTML pages

--- js/index.js
+++ js/index.js
@@ -43,3 +43,15 @@ function showSlides(n) {
 }
 
 /* End of JS for Carousel */
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
